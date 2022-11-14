63d1c9bea559233d97ef33daff29a133040332c2
ferfahl <fe.alves.fahl@gmail.com>
2022-11-14 12:56:09 -0300

    libraries included

--- sources/main.c
+++ sources/main.c
@@ -1,29 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: feralves <feralves@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/06 18:14:54 by feralves          #+#    #+#             */
-/*   Updated: 2022/11/14 12:55:13 by feralves         ###   ########.fr       */
+/*   Created: 2022/11/14 12:55:40 by feralves          #+#    #+#             */
+/*   Updated: 2022/11/14 12:55:58 by feralves         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
-
-//Libraries
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <string.h>
-
-//Structs
-
-//Functions
-
-#endif
+#include "pipex.h"
